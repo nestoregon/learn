ac12c96899363ee5117a58fd036e5edcaea612b8
Nestor <nestormorales1998@gmail.com>
2020-11-02 23:19:58 +0100

    commit

--- reinforced_learning/cartpole.py
+++ reinforced_learning/cartpole.py
@@ -7,7 +7,7 @@ for i in range(iterations): # for 1000 steps
     observation= env.reset() # reset to 0 the environment
     for t in range(100):
         env.render() # render the result, in order to see the progress
-        print(observation) # what is happening
+        # print(observation) # what is happening
         action = env.action_space.sample() # get a random action
         observation, reward, done, infor = env.step(action) # Action and see what the environment gets you (observation) the reward of your action, if we have achieved our goal and general information
         if done:
