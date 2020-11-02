7f55f85b9feb24b94682dfd5a1f4a3ff14fa8bb1
Nestor <nestormorales1998@gmail.com>
2020-11-02 23:11:36 +0100

    commit

--- reinforced_learning/cartpole.py
+++ reinforced_learning/cartpole.py
@@ -1,7 +1,16 @@
 import gym # import gym library
 env = gym.make('CartPole-v0') # this is the environment, from the library
-env.reset() # reset to 0 the environment
-for _ in range(1000): # for 1000 steps
-    env.render() # render the result, in order to see the progress
-    env.step(env.action_space.sample()) # Take a step in the environment
+
+
+
+for i in range(20): # for 1000 steps
+    observation= env.reset() # reset to 0 the environment
+    for t in range(100):
+        env.render() # render the result, in order to see the progress
+        print(observation) # what is happening
+        action = env.action_space.sample() # get a random action
+        observation, reward, done, infor = env.step(action) # Action and see what the environment gets you (observation) the reward of your action, if we have achieved our goal and general information
+        if done:
+            print("We're done here")
+            break
 env.close() # close the environment
