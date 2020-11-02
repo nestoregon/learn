e26129c848e8857b258dcc92d75024c6452dfb76
Nestor <nestormorales1998@gmail.com>
2020-11-02 22:53:05 +0100

    commit

--- reinforced_learning/cartpole.py
+++ reinforced_learning/cartpole.py
@@ -1,7 +1,7 @@
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
+import gym # import gym library
+env = gym.make('CartPole-v0') # this is the environment, from the library
+env.reset() # reset to 0 the environment
+for _ in range(1000): # for 1000 steps
+    env.render() # render the result, in order to see the progress
+    env.step(env.action_space.sample()) # Take a step in the environment
+env.close() # close the environment
